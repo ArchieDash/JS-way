268eab140b1af38547c0d71af7100cc939fa8909
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-02-01 08:58:02 +0300

    Temperature converter

---
+++ T_converter.js
@@ -0,0 +1,3 @@
+let celsius = parseInt(prompt("Temperature in celsius:"));
+let fahrenheit = celsius * 9/5 + 32;
+alert("Given temperature at celsius is equal to " + fahrenheit + " degrees fahrenheit");
