0a5417db269a31f4888ade4b5024e62512c00863
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-02-03 07:28:37 +0300

    Comparison of the given numbers

---
+++ number_comparison.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Enter the first number:"));
+let num2 = parseInt(prompt("Enter the second number:"));
+if (num1 > num2)  {
+    console.log("First number greater than second.");
+}
+else if (num1 < num2) {
+    console.log("First number less than second.");
+}
+else {
+    console.log("The numbers are equal");
+}
